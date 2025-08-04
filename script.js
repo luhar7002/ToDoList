c8ba6957b086b8c1ff1c5f12666cd0c71fed156f
luhar7002 <rahul2007sankar1971@gmail.com>
2025-08-04 14:59:14 +0530

    ToDoList Project Completed

---
+++ script.js
@@ -0,0 +1,30 @@
+var count=0;
+function addTask(){
+    count++;
+    var task=document.getElementById("input-text").value.trim();
+    if(task==="")
+    {
+        document.getElementById("input-text").style.border="2px solid red";
+        alert("Add your Task")
+        return;
+    }
+    var list=document.createElement("li");
+    list.innerHTML=`<p>task ${count}</p>    ${task} <button class="completed" onclick="toggleComplete(event)">completed</button>`
+    document.getElementById("tasks").append(list);
+    document.getElementById("input-text").value="";
+    document.getElementById("input-text").style.border="none";
+}
+function toggleComplete(event) {
+    event.target.parentElement.classList.toggle("done");
+    deleteTask(event);
+}
+function deleteTask(event)
+{
+    return event.target.remove();
+}
+document.getElementById("input-text").addEventListener("keypress",function(event){
+    if(event.key==='Enter')
+    {
+        addTask();
+    }
+});
